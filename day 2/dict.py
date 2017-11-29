bf9af85ba591629a8432e273aa39b198a5d42508
subhammalik <32295784+subhammalik@users.noreply.github.com>
2017-11-29 19:30:09 +0530

    Add files via upload

---
+++ day 2/dict.py
@@ -0,0 +1,16 @@
+#code1
+n1=dict([(11,111),(22,222),(33,333)])
+print(n1[22])
+n1[44]=444
+print(n1)
+print(len(n1))
+
+#code2
+n2=dict([(2,3),(22,33)])
+print(n2)
+n2[22]=333
+print(n2)
+del n2[2]
+print(n2)
+
+
---
+++ day 2/string.py
@@ -0,0 +1,16 @@
+#code1
+n1='python'
+n2=len(n1)
+print(n2)
+n3=len(n1[1:4])
+print(n3)
+n4=1.32
+n5=str(n4)
+n6=n5[3:]
+print(n6)
+
+#code2
+n7='lower'
+n8=n7.upper()
+n9=n8[1:4].lower()
+
