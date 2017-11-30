aecfad6941b83cb9d8cca2d327cf7ec6b4491821
subhammalik <32295784+subhammalik@users.noreply.github.com>
2017-12-01 02:11:18 +0530

    Add files via upload

---
+++ l1.py
@@ -0,0 +1,32 @@
+#day 4
+
+#list files of a particular directory
+# import os
+#
+# fw=open("C:/Users/SUBHAM/Desktop/foo/3.txt",'w')
+# fw.write('new stuff\ncool stuff\ngreat!')
+# fw.close()
+#
+# l=os.listdir("C:/Users/SUBHAM/Desktop/foo")
+# l.append('my.html')
+# print(l)
+#
+# try:
+#     for i in l:
+#         fr=open('C:/Users/SUBHAM/Desktop/foo/'+i)
+#         v=fr.read()
+#         w=len(v.split())
+#         s=len(v.splitlines())
+#         print('words in',i,'= ',w)
+#         print('lines in',i,'= ',s)
+#         print('avg words per line for ',i,'= ',float(w)/float(s))
+#
+#     fr.close()
+# except IOError:
+#     print('ERROR: Ooops!...the file does not exist.')
+
+
+
+
+
+
